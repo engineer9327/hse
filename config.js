f254ca6be5c50dd26e23dfb601cab0bda34f6242
engineer9327 <lastchancet2@gmail.com>
2026-09-05 17:12:33 +0900

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  KAKAO_APP_KEY:  "4275ca43e02ea72006df6b642b4d3b37",
-  KMA_API_KEY:    "sUmofD71SUyJqHw-9dlMqw",
-  KMA_CORS_PROXY: "https://kma-proxy.lastchancet2.workers.dev/?url="
-};
